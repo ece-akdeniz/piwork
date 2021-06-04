d29e6fe481b7792c686459c1a0a174fc3a20d0a2
Ece <75959278+ece-akdeniz@users.noreply.github.com>
2021-06-04 19:52:58 +0300

    Add files via upload

---
+++ piworks.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv('country_vaccination_stats.csv')
+
+data["daily_vaccinations"].fillna(data.groupby("country")["daily_vaccinations"].transform("min"), inplace=True)
+data["daily_vaccinations"].fillna(0, inplace=True)
