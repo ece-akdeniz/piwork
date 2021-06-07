a745fafccd9bd52c0df0c73f0ef5f456aeaf72c2
Ece <75959278+ece-akdeniz@users.noreply.github.com>
2021-06-07 14:04:38 +0300

    Add files via upload

---
+++ piworks5.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {"Device_Type": ["AXO145", "TRU151", "ZOD231", "YRT326", "LWR245"],
+		"Stats_Access_Link": ["<url>https://xcd32112.smart_meter.com</url>",
+							"<url>http://txh67.dia_meter.com</url>",
+							"<url>http://yT5495.smart_meter.com</url>",
+							"<url>https://ret323_TRu.crown.com</url>",
+							"<url>https://luwr3243. celcius. com</url>"]}
+
+df = pd.DataFrame(data)
+df["Stats_Access_Link"] = df["Stats_Access_Link"].apply(lambda x: x[13:-6])
+print(df)
